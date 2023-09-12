c254dbcaaf12bfe510ccced1d22581ecb53ec7ee
Eustus Mwirigi <60316624+Eustusmurea@users.noreply.github.com>
2023-09-12 09:47:21 +0300

    Update 100-my_int.py

--- 0x0A-python-inheritance/100-my_int.py
+++ 0x0A-python-inheritance/100-my_int.py
@@ -1,12 +1,15 @@
 #!/usr/bin/python3
-"""my int"""
+
+"""Defines a class MyInt that inherits from int."""
 
 
-class MyInt (int):
-    def __ev__(self, other):
-        """Return True if self and other not equal, else false"""
-        return int(self) != other
+class MyInt(int):
+    """Inverts int operators == and !=."""
 
-    def __pk__(self, other):
-        """Return True if self and other equal, else false"""
-        return int(self) == other
+    def __eq__(self, value):
+        """Overrides == opeartor with != behavior."""
+        return self.real != value
+
+    def __ne__(self, value):
+        """Override != operator with == behavior."""
+        return self.real == value
