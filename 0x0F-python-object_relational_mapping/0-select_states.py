848707ee12499d7f9ac6cd890778d4d5bfd5d71c
Eustus Mwirigi <60316624+Eustusmurea@users.noreply.github.com>
2023-11-21 12:40:36 +0300

    New tests

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -4,7 +4,27 @@ import sys
 import MySQLdb
 
 if __name__ == "__main__":
-    db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
-    c = db.cursor()
-    c.execute("SELECT * FROM `states`")
-    [print(state) for state in c.fetchall()]
+    # Check the number of command-line arguments
+    if len(sys.argv) != 4:
+        print("Usage: {} <username> <password> <database>".format(sys.argv[0]))
+        sys.exit(1)
+
+    try:
+        # Connect to the MySQL database
+        db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
+        c = db.cursor()
+
+        # Execute the SQL query
+        c.execute("SELECT * FROM `states`")
+
+        # Print the results
+        [print(state) for state in c.fetchall()]
+
+    except MySQLdb.Error as e:
+        print("MySQL Error {}: {}".format(e.args[0], e.args[1]))
+        sys.exit(1)
+
+    finally:
+        # Close the database connection
+        if 'db' in locals() and db:
+            db.close()
--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -4,9 +4,9 @@ import MySQLdb
 from sys import argv
 
 if __name__ == "__main__":
-    database = MySQLdb.connect(user=argv[1],
-                               passwd=argv[2],
-                               db=argv[3])
+    database = MySQLdb.connect(user='root',
+                               passwd='',
+                               db='hbtn_0e_0_usa')
     curs = database.cursor()
     curs.execute("SELECT * FROM states WHERE\
                   name LIKE BINARY '{}'\
